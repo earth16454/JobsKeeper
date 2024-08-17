69cea9bff0aae79ef7dae31b986e2804ae9621d6
9sep <thanaphong16454@gmail.com>
2024-08-17 22:49:57 +0700

    refactor

--- frontend/src/component/global/JKBreadcrumb.tsx
+++ frontend/src/component/global/JKBreadcrumb.tsx
@@ -4,45 +4,42 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
 export interface JKBreadcrumbProps {
-    staticBreadcrumb?: StaticBreadcrumb[];
+  staticBreadcrumb?: StaticBreadcrumb[];
 }
 
 export interface StaticBreadcrumb {
-    title: React.ReactNode;
-    path: string;
+  title: React.ReactNode;
+  path: string;
 }
 
 const JKBreadcrumb: React.FC<JKBreadcrumbProps> = ({ staticBreadcrumb }) => {
-    const navigate = useNavigate();
-    return (
-        <Card bodyStyle={{ padding: '8px 16px' }}>
-            <Breadcrumb>
-                {staticBreadcrumb && staticBreadcrumb.length > 0 ? (
-                    staticBreadcrumb.map((breadcrumb, index) => (
-                        <Breadcrumb.Item>
-                            <Typography.Link
-                                onClick={() => navigate(breadcrumb.path)}
-                                key={`breadcrumb_${index}`}
-                            >
-                                {breadcrumb.title}
-                            </Typography.Link>
-                        </Breadcrumb.Item>
-                    ))
-                ) : (
-                    <>
-                        <Breadcrumb.Item>
-                            <Typography.Link>
-                                <HomeOutlined style={{ marginRight: 8 }} />
-                                Home
-                            </Typography.Link>
-                        </Breadcrumb.Item>
-                        <Breadcrumb.Item>List</Breadcrumb.Item>
-                        <Breadcrumb.Item>App</Breadcrumb.Item>
-                    </>
-                )}
-            </Breadcrumb>
-        </Card>
-    );
+  const navigate = useNavigate();
+  return (
+    <Card styles={{ body: { padding: '8px 16px' } }}>
+      <Breadcrumb>
+        {staticBreadcrumb && staticBreadcrumb.length > 0 ? (
+          staticBreadcrumb.map((breadcrumb, index) => (
+            <Breadcrumb.Item>
+              <Typography.Link onClick={() => navigate(breadcrumb.path)} key={`breadcrumb_${index}`}>
+                {breadcrumb.title}
+              </Typography.Link>
+            </Breadcrumb.Item>
+          ))
+        ) : (
+          <>
+            <Breadcrumb.Item>
+              <Typography.Link>
+                <HomeOutlined style={{ marginRight: 8 }} />
+                Home
+              </Typography.Link>
+            </Breadcrumb.Item>
+            <Breadcrumb.Item>List</Breadcrumb.Item>
+            <Breadcrumb.Item>App</Breadcrumb.Item>
+          </>
+        )}
+      </Breadcrumb>
+    </Card>
+  );
 };
 
 export default JKBreadcrumb;
